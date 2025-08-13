aee7d3bfe27c4a1310d13595b1a73ae2dc005553
DoganM95 <doganmermertas@hotmail.de>
2025-08-13 15:29:46 +0200

    rm redundant flags in run command

--- rn.sh
+++ rn.sh
@@ -3,12 +3,10 @@ docker image rm wireguard-proxy
 docker build -t wireguard-proxy .
 docker run -d \
     --name wireguard-proxy \
+    --privileged \
     --cap-add=NET_ADMIN \
     --cap-add=SYS_MODULE \
     --device /dev/net/tun \
-    --sysctl net.ipv4.conf.all.src_valid_mark=1 \
-    --sysctl net.ipv4.ip_forward=1 \
-    --privileged \
     -p 8118:8118 \
     -e "DOMAINS_TO_RELAY=youtube.com,api.ipify.org,whatismyipaddress.com" \
     -v "/mnt2/homes/docker/Wireguard-Vpn-to-Proxy/surfshark_albania.conf:/etc/wireguard/wg0.conf" \
