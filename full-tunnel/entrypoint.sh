20eafd671f9f16916abd67c56d22a8dd06583352
DoganM95 <doganmermertas@hotmail.de>
2025-09-02 22:48:29 +0200

    try with wireguard again

--- full-tunnel/entrypoint.sh
+++ full-tunnel/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
-# Comment out WireGuard startup
-# wg-quick up /etc/wireguard/wg0.conf
+
+# Set DNS if provided via environment variables
+if [ -n "$DNS1" ]; then
+    echo "nameserver $DNS1" > /etc/resolv.conf
+fi
+
+# Start WireGuard VPN
+wg-quick up /etc/wireguard/wg0.conf
+
+# Start Privoxy in foreground
 exec privoxy --no-daemon /etc/privoxy/config
